2978702c28445d65e4d2c60946dae36c3fac3b5e
Hector <hafr40@gmail.com>
2022-06-23 16:26:54 -0500

    Context

--- src/contexts/cartContext.jsx
+++ src/contexts/cartContext.jsx
@@ -4,21 +4,42 @@ const CartContext = createContext([])
 
 export const useCartContext = () => useContext(CartContext)
 
-
 export const CartContextProvider = ({ children }) => {
-     
+  
     const [cart, setCart] = useState([])
 
+    /* const addToCart = (item, cantidad) => {
+        let newCart;
+        console.log("item-algo", item)
+        console.log("Cantidad", cantidad)
+        let product = cart.find(product => product.id === item.id);
+        console.log("Product", product)
+        if (product)
+        {
+            product.cantidad += cantidad;
+            newCart = [...cart];
+        }
+        else
+        {
+            product = {...item, cantidad: cantidad};
+            newCart = [...cart, product];
+        }
+        setCart(newCart)
+    } */
+    
+    
     const addToCart = (item) => {
         setCart([
             ...cart,
-            item
+            item 
         ])
     }
 
     const vaciarCarrito = () => {
         setCart([])
     }
+
+
 
     return (
         <CartContext.Provider 
