9e2a4d90ab0f602afe63649f1879c4372b2bd55c
Hector <hafr40@gmail.com>
2022-09-08 10:40:46 -0500

    Se modificó ItemList

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 function App() {
 
     //Se definen las props de las cards
-    let contenidoAutos = 'Aqui se mostrará el contenido de la categoría "Autos"'
-    let contenidoTrucks = 'Aqui se mostrará el contenido de la categoría "Trucks"'
-    let contenidoClasicos = 'Aqui se mostrará el contenido de la categoría "Clásicos"'
-    let contenidoMarioBros = 'Aqui se mostrará el contenido de la categoría "Super Mario Bros"'
+    //let contenidoAutos = 'Aqui se mostrará el contenido de la categoría "Autos"'
+    //let contenidoTrucks = 'Aqui se mostrará el contenido de la categoría "Trucks"'
+    //let contenidoClasicos = 'Aqui se mostrará el contenido de la categoría "Clásicos"'
+    //let contenidoMarioBros = 'Aqui se mostrará el contenido de la categoría "Super Mario Bros"'
 
     return (
 
         <>
             <NavBar />
             <Cuerpo />
-            <ItemListContainer contCardAutos={contenidoAutos} contCardTrucks={contenidoTrucks} contCardClasicos={contenidoClasicos} contCardMarioBros={contenidoMarioBros}/>
+            <ItemListContainer />
         </>
       
     );
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -23,34 +23,41 @@ const ItemListContainer = () => {
 
     return (
         <div>
-            { loading ? <h1>Cargando...</h1> : productos.map(prod => 
-                                                <div 
-                                                    className='col-md-4 p-1'    
-                                                    key={prod.id}                                            
-                                                >                        
-                                                <div className="card w-100 mt-5" >
-                                                <div className="card-header">
-                                                        {`${prod.model} - ${prod.series}`}
-                                                    </div>
-                                                    <div className="card-body">
-                                                        {<img src={prod.photo_url} alt='' className='w-50' />}
-                                                        {prod.stock}                                                            
-                                                    </div>
-                                                    <div className="card-footer">  
-                                                        <button className="btn btn-outline-primary btn-block">
-                                                            detalle del producto
-                                                        </button>                
-                                                    </div>
-                                                </div>
-                                                                                                                                
-                                            </div>
-
-                ) }
+            { loading ? <div className="text-center"><h3><br />Cargando productos...</h3></div> : productos.map(prod => 
+                <div className='col-md-4 p-1' key={prod.id} >                        
+                    <div className="card w-100 mt-5" >
+                        <div className="card-header text-center">
+                        <h4>Nombre: {`${prod.model}`}</h4>
+                        </div>
+                        <div className="card-body text-center">
+                            {<img src={prod.photo_url} alt='' className='w-50' />}                                                 
+                        </div>
+                        <div className="card-footer h5 text-center">
+                            Categoría: {prod.categoria}
+                            <br />
+                            Serie: {prod.series}
+                            <br />
+                            Año: ${prod.year}
+                            <br />
+                            Stock Disponible: {prod.stock} unidades
+                            <br />
+                            Precio: ${prod.precio} MXN
+                            <br />
+                            <br />
+                            <button className="btn btn-outline-primary btn-block">
+                                Detalle del producto
+                            </button>                
+                        </div>
+                    </div>                                                                                
+                </div>
+            ) }
         </div>
     )
 }
 
 export default ItemListContainer
+
+
 
 
 // import Card from 'react-bootstrap/Card';
