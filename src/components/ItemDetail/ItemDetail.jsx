8cf9e2a1a9024f957fdc8e2365ceb1a6b790ba79
Hector <hafr40@gmail.com>
2022-09-19 12:48:46 -0500

    Se eliminan comentarios de ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -37,29 +37,3 @@ const ItemDetail = ({producto}) => {
 }
 
 export default ItemDetail
-
-// import ItemCount from "../ItemCount/ItemCount"
-
-// const ItemDetail = ({producto}) => {
-
-//     return (
-//         <div className="row">
-//             <div className="col-md-6">
-//                 <img src={producto.photo_url} alt="" className="w-100" />
-//             </div>
-//             <div className="col-md-6">                
-//                 <h2>Modelo: {producto.model}</h2>
-//                 <h3>Categoría: {producto.categoria}</h3>
-//                 <h4>Serie: {producto.series}</h4>
-//                 <h4>Año: {producto.year}</h4>
-//                 <h4>Precio: ${producto.precio} MXN</h4>
-//                 <h4>Stock Disponible: {producto.stock}</h4>
-//                 <br />
-//                 <br />
-//                 <ItemCount stock={producto.stock} initial={1}/>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default ItemDetail
