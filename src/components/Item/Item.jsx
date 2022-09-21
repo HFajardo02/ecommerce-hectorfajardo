59e8842346dc6382173183c158c0f2f984bf9036
Hector <hafr40@gmail.com>
2022-09-21 16:56:46 -0500

    Cart View

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -34,33 +34,3 @@ const Item = ({prod}) => {
 }
 
 export default Item
-
-// import React from "react";
-// import Card from 'react-bootstrap/Card';
-// import CardGroup from 'react-bootstrap/CardGroup';
-
-// const Item = () => {
-
-//     return (
-
-//         <CardGroup className="container mt-5 text-center" key={producto.id}>
-//             <Card>
-//                 <Card.Img variant="top" src={productos.photo_url} alt='Hot Wheels'/>
-//                 <Card.Body>
-//                 <Card.Title>{productos.model}</Card.Title>
-//                 <Card.Text>{productos.series}</Card.Text>
-//                 <Card.Text>{productos.precio}</Card.Text>
-//                 <Card.Text>
-//                     <>
-//                         <ItemCount stock={productos.stock} initial={1}/>
-//                     </>
-//                 </Card.Text>
-//                 </Card.Body>
-//             </Card>
-//         </CardGroup>
-
-//     )
-
-// }
-
-// export default Item;
