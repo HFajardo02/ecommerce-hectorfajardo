be28fd2783f5eab1af241d6aa0f16941461a18be
Hector <hafr40@gmail.com>
2022-09-21 15:46:59 -0500

    Se elimina tabla en Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { useCartContext } from "../contexts/cartContext"
-import Table from 'react-bootstrap/Table';
+//import Table from 'react-bootstrap/Table';
 
 
 const Cart = () => {
@@ -11,62 +11,52 @@ const Cart = () => {
             {/* Comienza tabla */}      
              
             
-            <Table striped bordered hover size="sm">
+            {/* <Table>
                 <thead>
                     <tr>
                         <th>Producto</th>
                         <th>Nombre</th>
                         <th>Precio</th>
                         <th>Cantidad</th>
+                        <th>Subtotal</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <td>
+                    <tr>
                         {cart.map(item => (
-                        <td key={item.id}><img src={item.photo_url} className='w-25' alt="ilustracion"/></td>
-                        ))}
-                    </td>
-                    <td>
-                        {cart.map(item => (
-                        <td key={item.id}>{item.model}</td>
+                        <>
+                            <td key={item.id}><img src={item.photo_url} className='w-25 center-text' alt="ilustracion"/></td>
+                            <td key={item.id}>{item.model}</td>
+                            <td key={item.id}>${item.precio} MXN</td>
+                            <td key={item.id}>{item.cantidad} Pzs</td>
+                            <td key={item.id}>{item.precio}*{item.cantidad} </td>
+                        </>
                         ))} 
-                    </td>
-                    <td>
-                        {cart.map(item => (
-                        <td key={item.id}>${item.precio} MXN</td>
-                        ))} 
-                    </td>
-                    <td>
-                        {cart.map(item => (
-                        <td key={item.id}>{item.cantidad} Pzs</td>
-                        ))} 
-                    </td>
+                    </tr>
                 </tbody>
-            </Table>
+            </Table> */}
             
             {/* Finaliza tabla */}
 
-            {/* <ul>
+            <ul>
                 {
                     cart.map(item => <li key={item.id}>
                         <div className="w-75">
                             <img src={item.photo_url} className='w-25' alt="ilustracion"/>
-                            <b> Nombre:</b> {item.model} 
-                            <b> Precio:</b> ${item.precio} MXN 
-                            <b> Cantidad:</b> {item.cantidad} Unidades 
+                            <b>   Nombre:</b> {item.model} 
+                            <b>   Precio:</b> ${item.precio} MXN 
+                            <b>   Cantidad:</b> {item.cantidad} Unidades 
                         </div>
                     </li>  )
                 }
-            </ul> */}
-            <div className="mt-10">
-                <button className="btn btn-secondary" onClick={vaciarCarrito}>Vaciar Carrito</button>
-            </div>
-            <div className="mt-5">
+            </ul>
+            <div className="container mt-10 text-center">
                 <Link to='/' >
-                    <button className="btn btn-secondary" onClick={()=>console.log('Ir al Inicio') }>Seguir comprando</button>
+                    <button className="btn btn-warning offset-md-1" onClick={()=>console.log('Ir al Inicio') }>Seguir comprando</button>
                 </Link>
+                <button className="btn btn-danger offset-md-1" onClick={vaciarCarrito}>Vaciar Carrito</button>
+                <button className="btn btn-dark offset-md-1">Finalizar Compra</button>
             </div>
-
         </div>
     )
 }
