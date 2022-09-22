9c9799f5a728a11506b7cd64cf87863179c79ae0
Hector <hafr40@gmail.com>
2022-09-22 15:21:02 -0500

    CartView

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -28,33 +28,3 @@ const NavBar = () => {
 }
 
 export default NavBar
-
-
-// import Navbar from 'react-bootstrap/Navbar';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-
-// import CartWidget from '../CartWidget/CartWidget';
-
-// const NavBar = () => {
-//   return (
-//         <>
-
-//             <CartWidget />
-
-//             <Navbar bg="dark" variant="dark">
-//                 <Container>
-//                     <Nav className="me-auto">
-//                         <Nav.Link href="# ">Autos</Nav.Link>
-//                         <Nav.Link href="# ">Trucks</Nav.Link>
-//                         <Nav.Link href="# ">Clásicos</Nav.Link>
-//                         <Nav.Link href="# ">Super Mario Bros</Nav.Link>
-//                     </Nav>
-//                 </Container>
-//             </Navbar>
-
-//         </>
-//     )
-// }
-
-// export default NavBar
